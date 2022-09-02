f8a52f8a3756251ed73527d566d79150ca823ff4
ibrahim-boubakri <107005643+ibrahim-boubakri@users.noreply.github.com>
2022-09-02 15:09:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavbarCustom from "./components/NavbarCustom";
+import Home from "./components/Home";
+import List from "./components/List";
+import DetailsArticle from "./components/DetailsArticle";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "T-shrt",
+      price: 55,
+      image:
+        "http://www.nielsenanimal.com/wp-content/uploads/2018/09/womens-nike-pure-t-shirt-pink-t-shirts_1.jpg",
+    },
+    {
+      id: 2,
+      name: "Shorts",
+      price: 96,
+      image:
+        "http://www.nielsenanimal.com/wp-content/uploads/2018/09/mens-nike-2in1-5-inch-flex-running-shorts-grey-shorts.jpg",
+    },
+    {
+      id: 3,
+      name: "men-shoes",
+      price: 196,
+      image:
+        "http://www.nielsenanimal.com/wp-content/uploads/2018/09/mens-nike-a-m-infuriate2-m-s91-blackblackwht-trainers.jpg",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavbarCustom />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<List products={products} />} />
+        <Route path="/products/:id" element={<DetailsArticle products={products}/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
